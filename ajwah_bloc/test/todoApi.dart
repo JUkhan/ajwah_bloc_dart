ef44173fa944cd5734fd84c37395db0da79867e8
jukhan <jukhan@Kamruls-MacBook-Pro.local>
2019-05-19 10:32:21 +0600

    doc

--- ajwah_bloc/test/todoApi.dart
+++ ajwah_bloc/test/todoApi.dart
@@ -1,42 +1,28 @@
 import 'TodoState.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
 import 'dart:async';
 
 class TodoApi {
   static String baseUrl = 'https://jsonplaceholder.typicode.com/todos';
 
   static Future<List<Todo>> getTodos({int limit = 5}) async {
-    var response = await http.get(TodoApi.baseUrl + '?_limit=${limit}');
-    var jsonResponse = convert.jsonDecode(response.body);
-
-    return (jsonResponse as List)
-        .cast<Map<String, dynamic>>()
-        .map<Todo>((json) => Todo.fromJson(json))
-        .toList();
+    return [
+      Todo(id: 201, title: 'todo.title', completed: false),
+      Todo(id: 2011, title: 'todo.title', completed: false),
+      Todo(id: 2012, title: 'todo.title', completed: false),
+      Todo(id: 2013, title: 'todo.title', completed: false),
+      Todo(id: 2014, title: 'todo.title', completed: false)
+    ];
   }
 
   static Future<Todo> addTodo(Todo todo) async {
-    var response =
-        await http.post(TodoApi.baseUrl, body: convert.jsonEncode(todo));
-    var jsonResponse = convert.jsonDecode(response.body);
-
-    return Todo(
-        id: (jsonResponse['id'] as int),
-        title: todo.title,
-        completed: todo.completed);
+    return Todo(id: 201, title: todo.title, completed: todo.completed);
   }
 
   static Future<Todo> updateTodo(Todo todo) async {
-    var response = await http.put(TodoApi.baseUrl + '/${todo.id}',
-        body: convert.JsonCodec().encode(todo));
-    //var jsonResponse = convert.jsonDecode(response.body);
-    print(response.body);
     return todo;
   }
 
   static Future<Todo> removeTodo(Todo todo) async {
-    await http.delete(TodoApi.baseUrl + '/${todo.id}');
     return todo;
   }
 }
